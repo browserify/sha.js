3d8995821e8da1cbf85114589e15843b371b9285
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-15 13:08:56 +0700

    inject Buffer dep, so can test with different implementations

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var exports = module.exports = function (alg) {
   return new Alg()
 }
 
+var Buffer = require('buffer').Buffer
+var Hash   = require('./hash')(Buffer)
+
 exports.sha =
-exports.sha1 = require('./sha1')
-exports.sha256 = require('./sha256')
+exports.sha1 = require('./sha1')(Buffer, Hash)
+exports.sha256 = require('./sha256')(Buffer, Hash)
