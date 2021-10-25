86c0e2ceaee360c7545f49a6d6aa849958207b89
Jimmy Wärting <jimmy@warting.se>
2021-10-25 09:44:13 +0200

    switch to class

--- sha.js
+++ sha.js
@@ -6,7 +6,6 @@
  * operation was added.
  */
 
-var inherits = require('inherits')
 var Hash = require('./hash')
 var Buffer = require('safe-buffer').Buffer
 
@@ -16,23 +15,67 @@ var K = [
 
 var W = new Array(80)
 
-function Sha () {
-  this.init()
-  this._w = W
+class Sha extends Hash {
+  constructor () {
+    super(64, 56)
 
-  Hash.call(this, 64, 56)
-}
+    this.init()
+    this._w = W
+  }
 
-inherits(Sha, Hash)
+  init() {
+    this._a = 0x67452301
+    this._b = 0xefcdab89
+    this._c = 0x98badcfe
+    this._d = 0x10325476
+    this._e = 0xc3d2e1f0
 
-Sha.prototype.init = function () {
-  this._a = 0x67452301
-  this._b = 0xefcdab89
-  this._c = 0x98badcfe
-  this._d = 0x10325476
-  this._e = 0xc3d2e1f0
+    return this
+  }
 
-  return this
+  _update(M) {
+    var W = this._w
+
+    var a = this._a | 0
+    var b = this._b | 0
+    var c = this._c | 0
+    var d = this._d | 0
+    var e = this._e | 0
+
+    for (var i = 0; i < 16; ++i)
+      W[i] = M.readInt32BE(i * 4)
+    for (; i < 80; ++i)
+      W[i] = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16]
+
+    for (var j = 0; j < 80; ++j) {
+      var s = ~~(j / 20)
+      var t = (rotl5(a) + ft(s, b, c, d) + e + W[j] + K[s]) | 0
+
+      e = d
+      d = c
+      c = rotl30(b)
+      b = a
+      a = t
+    }
+
+    this._a = (a + this._a) | 0
+    this._b = (b + this._b) | 0
+    this._c = (c + this._c) | 0
+    this._d = (d + this._d) | 0
+    this._e = (e + this._e) | 0
+  }
+
+  _hash() {
+    var H = Buffer.allocUnsafe(20)
+
+    H.writeInt32BE(this._a | 0, 0)
+    H.writeInt32BE(this._b | 0, 4)
+    H.writeInt32BE(this._c | 0, 8)
+    H.writeInt32BE(this._d | 0, 12)
+    H.writeInt32BE(this._e | 0, 16)
+
+    return H
+  }
 }
 
 function rotl5 (num) {
@@ -47,48 +90,6 @@ function ft (s, b, c, d) {
   if (s === 0) return (b & c) | ((~b) & d)
   if (s === 2) return (b & c) | (b & d) | (c & d)
   return b ^ c ^ d
-}
-
-Sha.prototype._update = function (M) {
-  var W = this._w
-
-  var a = this._a | 0
-  var b = this._b | 0
-  var c = this._c | 0
-  var d = this._d | 0
-  var e = this._e | 0
-
-  for (var i = 0; i < 16; ++i) W[i] = M.readInt32BE(i * 4)
-  for (; i < 80; ++i) W[i] = W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16]
-
-  for (var j = 0; j < 80; ++j) {
-    var s = ~~(j / 20)
-    var t = (rotl5(a) + ft(s, b, c, d) + e + W[j] + K[s]) | 0
-
-    e = d
-    d = c
-    c = rotl30(b)
-    b = a
-    a = t
-  }
-
-  this._a = (a + this._a) | 0
-  this._b = (b + this._b) | 0
-  this._c = (c + this._c) | 0
-  this._d = (d + this._d) | 0
-  this._e = (e + this._e) | 0
-}
-
-Sha.prototype._hash = function () {
-  var H = Buffer.allocUnsafe(20)
-
-  H.writeInt32BE(this._a | 0, 0)
-  H.writeInt32BE(this._b | 0, 4)
-  H.writeInt32BE(this._c | 0, 8)
-  H.writeInt32BE(this._d | 0, 12)
-  H.writeInt32BE(this._e | 0, 16)
-
-  return H
 }
 
 module.exports = Sha
